7e3d164372c9db3146a1530b61d11258029d7c12
Kowal64 <kowalczukagustin@gmail.com>
2021-08-31 02:08:48 -0300

    ej

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router()
 
+router.get('/inicio',(req,res)=>{
+    res.send('<h1> Que comience el juego </h1>');
+})
 
 module.exports = router
