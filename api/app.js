96323f9dbef770500a5499d0b025951a0ccac516
Kowal64 <kowalczukagustin@gmail.com>
2021-08-31 00:46:31 -0300

    carga de back

--- api/app.js
+++ api/app.js
@@ -1,6 +1,25 @@
 const express = require ('express');
+const bodyParser = require('body-parser')
 const app = express();
 
-const port = 3000
+const routes = require ('./routes/index.js')
 
-app.listen(port,()=> console.log('Server on port',port))
+//settings
+app.set('port', process.env.PORT || 3000);
+
+
+//middlewares
+app.use((req,res,next)=>{
+    console.log(`${req.url} - ${req.method}`);
+    next();
+})
+app.use(bodyParser,json());
+
+// routes
+app.use(routes)
+
+
+//start the server
+app.listen(app.get('port'),()=> {
+    console.log('Server on port',port)
+});
