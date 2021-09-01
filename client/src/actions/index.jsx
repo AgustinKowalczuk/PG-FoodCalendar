87144d82baf9a479d5e55a440291e491efd5b163
Lautaro124 <59624470+Lautaro124@users.noreply.github.com>
2021-09-01 17:48:44 -0300

    Change 0.1 Lautaro

--- client/src/actions/index.jsx
+++ client/src/actions/index.jsx
@@ -1,27 +1,37 @@
 import axios from "axios";
-import {GET_RECIPES, GET_INGREDIENTS}from './constants'
-import {RECIPES_URL, INGREDIENTS_URL } from '../routes'
-
-
+import { GET_RECIPES, GET_INGREDIENTS, SEARCH_RECIPES } from "./constants";
+import { RECIPES_URL, INGREDIENTS_URL } from "../routes";
 
 export function getRecipes() {
-        return async function (dispatch){
-                const recipes= await axios.get(RECIPES_URL);
-                return dispatch ({
-                        type: GET_RECIPES,
-                        payload:recipes.data
-                });
-        };
+
+  return async function (dispatch) {
+    const recipes = await axios.get(RECIPES_URL);
+    return dispatch({
+      type: GET_RECIPES,
+      payload: recipes.data,
+    });
+  };
 }
 
-export function getIngredients(){
-        return async function (dispatch){
-                const ingredients = await axios.get (INGREDIENTS_URL);
-                return dispatch ({
-                        type: GET_INGREDIENTS,
-                        payload: ingredients.data
-                })
-        }
+export function getIngredients() {
+
+  return async function (dispatch) {
+    const ingredients = await axios.get(INGREDIENTS_URL);
+    return dispatch({
+      type: GET_INGREDIENTS,
+      payload: ingredients.data,
+    });
+  };
 }
 
+// Despues voy a realizar cambios en la function y el el axios
+export function searchRecipes(id) {
 
+  return async function (dispatch) {
+    const filtRecipes = await axios.get(RECIPES_URL + `/${id}`);
+    return dispatch({
+      type: SEARCH_RECIPES,
+      payload: filtRecipes.data,
+    });
+  };
+}
