82014b4be7a04183babc5896d284a94da3dc9272
Natalia-Denis <natalia,s,denis@gmail.com>
2021-08-31 23:26:43 -0300

    update component

--- client/src/reducer/index.jsx
+++ client/src/reducer/index.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
 
-export default function reducer() {
-        return (
-                <div>
-                        
-                </div>
-        )
+import{GET_RECIPES, GET_INGREDIENTS }from '../actions/constants'
+var initialState={
+        recipes:[],
+        ingredients:[],
+
 }
+
+
+function reducer(state = initialState, action) {
+        switch (action.type){
+                case GET_RECIPES:
+               return {
+                ...state,
+                recipes: action.payload,
+               };
+               case GET_INGREDIENTS:
+               return {
+                ...state,
+                ingredients: action.payload,
+               }
+               default: return state;
+        }
+        
+}
+
+export default reducer;
