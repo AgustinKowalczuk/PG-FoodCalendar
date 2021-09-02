8cfed624a2fe696b7df5981fb58c110cb5548673
Kowal64 <kowalczukagustin@gmail.com>
2021-09-01 23:46:24 -0300

    Revert "ej"
    This reverts commit 7e3d164372c9db3146a1530b61d11258029d7c12.

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const router = express.Router()
 
-router.get('/inicio',(req,res)=>{
-    res.send('<h1> Que comience el juego </h1>');
-})
 
 module.exports = router
