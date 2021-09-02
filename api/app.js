c8ddaec20ec1fe16c8e0d0592256afefdc05b89a
Carlos7979 <carloscorredorwg@gmail.com>
2021-09-01 23:16:03 -0400

    add url to env file for remote mongodb  connection, connect express to mongoose and create a basic intial ingredient schema in order to test db functionality

--- api/app.js
+++ api/app.js
@@ -1,25 +1,43 @@
+require('dotenv').config();
 const express = require ('express');
-const bodyParser = require('body-parser')
-const app = express();
-
+const package = require('./package.json');
 const routes = require ('./routes/index.js')
+const mongoose = require('mongoose');
 
 //settings
-app.set('port', process.env.PORT || 3000);
-
-
-//middlewares
-app.use((req,res,next)=>{
-    console.log(`${req.url} - ${req.method}`);
+const { env: { PORT, MONGO_URL: url }, argv: [, , port = PORT || 3001], } = process;
+const app = express();
+const cors = (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
     next();
-})
-app.use(bodyParser.json());
+}
 
-// routes
-app.use(routes)
+(async () => {
+    await mongoose.connect(url, { useNewUrlParser: true })
+ 
+    // express
+    app.set('port', PORT || 3001);
+    app.use(cors);
+    app.use(express.json());
+ 
+    // routes
+    app.use(routes);
+
+    app.use(function (req, res, next) {
+        res.status(404).json({ error: 'Not found.' })
+    })
+
+    app.listen(port, () => console.log(`${package.name} ${package.version} up on port ${port}`))
+})()
+
+
+
+
+
 
 
 //start the server
-app.listen(app.get('port'),()=> {
-    console.log('Server on port',app.get('port'))
-});
+// app.listen(app.get('port'),()=> {
+//     console.log('Server on port',app.get('port'))
+// });
