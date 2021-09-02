e7b466f34b8619f35613d98c4b1c63503b89f1e1
Jordan <jordan.olaves91@gmail.com>
2021-09-02 10:55:40 -0500

    changesOnCards

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {getRecipes}from '../../actions/index'
+import Cards from '../Cards/Cards';
 
 export default function Home (){
-        const dispatch = useDispatch()
-        const allRecipes = useSelector((state)=>state.recipes)
+        // const dispatch = useDispatch()
+        // const allRecipes = useSelector((state)=>state.recipes)
+        // console.log(allRecipes)
 
-        useEffect(()=>{
-                dispatch(getRecipes());
-        },[])
+        // useEffect(()=>{
+        //         dispatch(getRecipes());
+        // },[])
 
         return (
                 <div>
-               Soy Home         
+                <Cards/>
                 </div>
         )
 }
