f7183debce3c30e7b1ced68420c5070e0fadf33e
Jocho94 <jocho1990@hotmail.com>
2021-09-03 01:46:52 -0300

    todas las rutas de ingredientes

--- api/routes/ingredient/delete_ingredient.js
+++ api/routes/ingredient/delete_ingredient.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
+const models = require('../../models/models');
+const { Ingredient } = models;
 
-router.delete('/ingredients',(req,res)=>{
-    res.send('Ingredientes esta conectado (delet)')
+router.delete('/ingredients/:name', async (req, res, next) => {
+    const { name } = req.params;
+    try {
+        const elem = await Ingredient.findOne({ name });
+        if (!elem) { return res.status(404).send(`el ingrediente con el nombre ingresado no existe`) }
+        const remove = await Ingredient.findByIdAndRemove(elem._id);
+        return res.json(remove);
+    } catch (error) {
+        next(error)
+    }
 })
 
 module.exports = router
--- api/routes/ingredient/put_ingredient.js
+++ api/routes/ingredient/put_ingredient.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
+const models = require('../../models/models');
+const { Ingredient } = models;
 
-router.put('/ingredients',(req,res)=>{
-    res.send('Ingredientes esta conectado (put)')
+router.put('/ingredients/:name', async (req, res, next) => {
+    const { name } = req.params;
+    const body = req.body;
+    try {
+        const elem = await Ingredient.findOne({ name });
+        if (!elem) { return res.status(404).send(`el ingrediente con el nombre ingresado no existe`) }
+        const update = await Ingredient.findByIdAndUpdate(elem._id, body);
+        return res.json(update);
+    } catch (error) {
+        next(error)
+    }
 })
 
 module.exports = router
