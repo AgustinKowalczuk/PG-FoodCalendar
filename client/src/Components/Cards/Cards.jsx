4cdb0a3df66fd39a36102c88c0cd9fee323cffc8
Lautaro124 <59624470+Lautaro124@users.noreply.github.com>
2021-09-03 16:38:33 -0300

    Change 0.5

--- client/src/Components/Cards/Cards.jsx
+++ client/src/Components/Cards/Cards.jsx
@@ -22,7 +22,7 @@ export default function Cards(){
                 <div class="card" id={style.carData}>
                     <img class="card-img-top" src={e.img} alt="No sé encuentra la imagen" />
                     <div class="card-body" >
-                        <h1 class="card-title" >{e.name}</h1>
+                        <h1 class="card-title" >{e.name.toUpperCase()}</h1>
                         <h4 class="card-text" >Dificultad: {e.difficulty}</h4>
                     </div>       
                 </div>
