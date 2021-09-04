88f153006b9b0ec215458c3f71b8fa801546b012
Lautaro124 <59624470+Lautaro124@users.noreply.github.com>
2021-09-04 18:44:50 -0300

    Change 1.0

--- client/src/Components/CreateRecipe/CreateRecipe.jsx
+++ client/src/Components/CreateRecipe/CreateRecipe.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import style from "../../Styles/StyleFrom.module.css";
+import { createRecipe, getIngredients } from "../../actions/index"
 import { useFormik } from "formik";
+import { useDispatch, useSelector } from 'react-redux'
+
 
 export default function CreateRecipe() {
+  
+  
+  
+const dispatch = useDispatch()
+
+const ingre = useSelector((state) => state.ingredients)
+
+
+  useEffect(() =>{
+    dispatch(getIngredients())
+  }, [dispatch])
 
   const initialValues = {
     name: '',
     preparation: '',
-    difficult: 'Easy',
-    ingredients: '',
-    image: '',
+    difficulty: 'Easy',
+    ingredients: [],
+    img: '',
   }
 
   const validate = (values) => {
@@ -27,14 +41,15 @@ export default function CreateRecipe() {
       error.preparation = "Not String";
     }
     
-    if (!values.image) {
-      error.image = "Required";
+    if (!values.img) {
+      error.img = "Required";
     }
     
     return error;
   }
 
-  const onSubmit = async (values) => {
+  const onSubmit =  (values) => {
+    dispatch(createRecipe(formik.values))
     console.log('Values submit', values);
   }
   
@@ -43,6 +58,10 @@ export default function CreateRecipe() {
     onSubmit,
     validate
   })
+  
+  const onDelete = (event) => {
+    formik.values.ingredients = formik.values.ingredients.filter(e => e === event.target.value)
+  }
 
   return (
     <div class={style.centrado}>
@@ -66,24 +85,45 @@ export default function CreateRecipe() {
 
             <div class="mb-3">
               <label class="form-label">Ingredients</label>
-              <select onChange={formik.handleChange} name='ingredients' id="disabledSelect" class="form-select">
-                <option name='ingredients' value ="Agua" >Agua</option>
-                <option name='ingredients' value ="Barro">Barro</option>
-                <option name='ingredients' value ="Harina">Harina</option>
+              <select onChange={formik.handleChange} name={`ingredients[${formik.values.ingredients.length}].ingredient`} id="disabledSelect" class="form-select">
+                {
+                  ingre?.map(e => {
+                    return <option name='ingredients' value={e.name}>{e.name}</option>;
+                  })
+                }
+              </select>
+              <div class={style.buttonsRemove}>
+                {
+                  formik.values.ingredients.length > 0 && formik.values.ingredients.map((e) => {
+                    return (
+                      <div class={style.grid}>
+                        <input class={style.inputGrid} onChange={formik.handleChange} name={e.amount} />
+                        <select class={style.selectGrid} onChange={formik.handleChange} name={e.unit} id="disabledSelect" class="form-select">
+                          <option name={e.unit}>G</option>
+                          <option name={e.unit}>KG</option>
+                        </select>
+                        <h5 onClick={(x) => onDelete(x)} 
+                          value={e.ingredient} class={style.button}>
+                            {e.ingredient}
+                        </h5>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </div>
+
+            <div class="mb-3">
+              <label class="form-label">difficulty</label>
+              <select onChange={formik.handleChange} name='difficulty' class="form-control">
+                <option name='difficulty' value="Easy"> Easy </option>
+                <option name='difficulty' value="Medium"> Medium </option>
+                <option name='difficulty' value="Hard"> Hard </option>
               </select>
             </div>
 
             <div class="mb-3">
-              <label class="form-label">Difficult</label>
-              <select onChange={formik.handleChange} name='difficult' class="form-control">
-                <option name='difficult' value="Easy"> Easy </option>
-                <option name='difficult' value="Medium"> Medium </option>
-                <option name='difficult' value="Hard"> Hard </option>
-              </select>
-            </div>
-
-            <div class="mb-3">
-              <label class="form-label">Preparation</label>
+              <label class="form-label">preparation</label>
               <textarea 
                 onChange={formik.handleChange}
                 value={formik.values.preparation} 
@@ -101,19 +141,19 @@ export default function CreateRecipe() {
             </div>
 
             <div class="mb-3">
-              <label class="form-label">Image</label>
+              <label class="form-label">image</label>
               <input 
                 onChange={formik.handleChange}
-                value={formik.values.image}
+                value={formik.values.img}
                 onBlur={formik.handleBlur}
                 class="form-control"
-                name="image"
+                name="img"
                 type="text"
                 placeholder="write here..."
               />
               {
-                formik.errors.image && formik.touched.image === true ? 
-                 <div class='cosoForm'><span>{formik.errors.image}</span></div> : null
+                formik.errors.img && formik.touched.img === true ? 
+                 <div class='cosoForm'><span>{formik.errors.img}</span></div> : null
               }
             </div>
             <div class="col-auto">
