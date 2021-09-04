b66fc1e15f61d61ef9e844a0bbf27716ac484512
Natalia-Denis <natalia,s,denis@gmail.com>
2021-09-04 13:25:09 -0300

    home&cards

--- client/src/Components/Cards/Cards.jsx
+++ client/src/Components/Cards/Cards.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { getRecipes } from '../../actions';
-import { useEffect} from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import {Link} from 'react-router-dom'
-import {getDetail}from '../../actions'
+import { useEffect, useState} from "react";
+import { useSelector, useDispatch } from 'react-redux';
 import style from '../../Styles/StyleCards.module.css';
+import { Link } from 'react-router-dom';
+import { getDetail } from '../../actions/index'
+
 import easy from '../../Image/easy.png'
 import hard from '../../Image/hard.png'
 import medium from '../../Image/medium.png'
 
+import Pagination from '../Pagination/Pagination'
+
 export default function Cards(){
     //Traigo todo
-    const dispatch = useDispatch();
     const allRecipes = useSelector((state) => state.recipes)
+    //Despacho
+    const dispatch = useDispatch()
+    //Para el paginado
+    const [currentPage, setCurrentPage] = useState(1);
+    const [recipesPerPage, setRecipesPerPage] = useState(6);
+    const lastRecipeIndex = currentPage * recipesPerPage;
+    const firstRecipeIndex = lastRecipeIndex - recipesPerPage;
+    const currentRecipes = allRecipes.slice(firstRecipeIndex, lastRecipeIndex);
+    const paginado = (pageNumber) =>{
+        setCurrentPage(pageNumber)
+    }
 
     //Lo despacho
     useEffect(() => {
@@ -20,26 +33,32 @@ export default function Cards(){
     }, [dispatch])
     
     //Existen recetas? Mandale mecha.
+    
     return (
-        <div class={style.content} >
-        {allRecipes?.map((e) => {
+        <div class={style.content}>
+        <Pagination
+        recipesPerPage = {recipesPerPage}
+        allRecipes = {allRecipes.length}
+        paginado = {paginado}
+        />
+        {currentRecipes?.map((e) => {
             return(
                 <div class="card" id={style.carData} Key={e.id}>
                     <Link to={`/recipe/${e.id}`}
               onClick={() => dispatch(getDetail(e.id))} id={style.normal}>
                     <img class="card-img-top" src={e.img} alt="No sé encuentra la imagen" />
                     <div class="card-body" >
-                        <h1 class="card-title" id={style.normal}>{e.name}</h1>
-                     <div>{e.difficulty ==='Fácil'?
+                        <h1 class="card-title" >{e.name.toUpperCase()}</h1>
+                        <div>{e.difficulty ==='Fácil'?
                             <h4 class="card-text" id={style.normal}>Dificultad:{e.difficulty} 
                             <img width="100px"  src= {easy} alt='easy'/></h4>
                             : e.difficulty==='Moderado'?
                                 <h4 class="card-text" id={style.normal} >Dificultad:{e.difficulty} 
                                 <img width="100px" src= {medium} alt='medium'/></h4>
                                 :<h4 class="card-text" id={style.normal} >Dificultad:{e.difficulty} 
-                                    <img width="100px" src= {hard} alt='hard'/></h4>
-                           }                  
-                      </div> 
+                                <img width="100px" src= {hard} alt='hard'/></h4>
+                       }                  
+                        </div>       
                     </div>       
                 </Link>
                 </div>
--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-
 import Cards from '../Cards/Cards';
+import calendar from '../../Image/Menu_semanal.jpg'
 
 
-export default function Home (){       
+export default function Home() {
         return (
                 <div>
-                 <Cards/>
+                 <h2>Plan your meals </h2>
+                 <img src= {calendar} alt='medium'/>
+                <h2 >We have these recipes for you</h2>
+                    <Cards />
                 </div>
         )
 }
