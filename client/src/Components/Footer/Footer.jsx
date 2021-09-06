ee34fe5119b961b353a634edafaf6a76346f94fb
Kowal64 <kowalczukagustin@gmail.com>
2021-09-06 07:35:14 -0300

    Logo Español

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar"
-import logo from '../../Image/Logo_RC.jpg'
+// import logo from '../../Image/Logo_RC.jpg'
 import style from "../../Styles/StyleNav.module.css"
-
+import logoSP from '../../Image/Logo_sp.png'
 
 
 export default function Footer() {
@@ -12,7 +12,7 @@ export default function Footer() {
     <div id={style.nav} class="navbar navbar-light bg-light">
       <div class="container-fluid">
         <Link class="navbar-brand" to = '/'>
-          <img width="55%" height="45%" src= {logo} alt='logo'/>
+          <img width="55%" height="45%" src= {logoSP} alt='logo'/>
         </Link>
         <h6>© Copyright 2021 - Henry - All Rights Reserved</h6>
         <SearchBar/>
--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar"
-import logo from '../../Image/Logo_RC.jpg'
+// import logo from '../../Image/Logo_RC.jpg'
 import style from "../../Styles/StyleNav.module.css"
-
+import logoSP from '../../Image/Logo_sp.png'
 
 
 export default function Nav() {
@@ -12,7 +12,7 @@ export default function Nav() {
     <div id={style.nav} class="navbar navbar-light bg-light">
       <div class="container-fluid">
         <Link class="navbar-brand" to = '/'>
-          <img width="55%" height="45%" src= {logo} alt='logo'/>
+          <img width="55%" height="45%" src= {logoSP} alt='logo'/>
         </Link>
         <div id={style.mobilecenter} class="navbar-nav me-auto mb-2 mb-lg-0">
           <Link id={style.link} class="nav-link active" to="/create/recipe">Crear receta</Link>
