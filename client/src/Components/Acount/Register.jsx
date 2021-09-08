0e6eed341095dd9d2d59cc29812c915936ae3f1b
Lautaro124 <59624470+Lautaro124@users.noreply.github.com>
2021-09-08 12:24:22 -0300

    Creacion de componenetes

---
+++ client/src/Components/Acount/Register.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Formik, Field, Form } from 'formik'
+
+export default function Register() {
+
+    const initialValues= {
+        name: '',
+        email: '',
+    }
+
+    return (
+        <div>
+            <h1>REGISTER</h1>
+        </div>
+    )
+}
---
+++ client/src/Components/ShopingCart/ShopingCart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ShopingCart() {
+    return (
+        <div>
+            <h1>ShopingCart</h1>
+        </div>
+    )
+}
