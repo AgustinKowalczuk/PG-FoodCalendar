9128dd77f96e79864bdd6ccc18f96a50600d617b
Jordan <jordan.olaves91@gmail.com>
2021-09-13 13:14:04 -0500

    MinorChanges

--- client/src/Components/ShopingCart/Calendar/Calendar.jsx
+++ client/src/Components/ShopingCart/Calendar/Calendar.jsx
@@ -2,8 +2,6 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCalendar, getCalendarDetail } from '../../../actions'
 import { Link } from 'react-router-dom';
-import CalendarDetail from './CalendarDetail';
-
 
 export default function Calendar() {
     const state = useSelector(state => state.calendary)
