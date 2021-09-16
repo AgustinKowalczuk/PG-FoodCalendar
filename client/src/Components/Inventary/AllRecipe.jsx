873536c1f6d95ef6a34878151a74a670d725ee01
Lautaro124 <59624470+Lautaro124@users.noreply.github.com>
2021-09-16 15:47:05 -0300

    Mejora estetica

--- client/src/Components/Inventary/AllRecipe.jsx
+++ client/src/Components/Inventary/AllRecipe.jsx
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getRecipes } from '../../actions';
 import Cards from '../Cards/Cards'
 import MidNav from '../Nav/midNav/midNav';
-import SearchBar from '../SearchBar/SearchBar';
 
 export default function AllRecipe() {
         const dispatch = useDispatch()
@@ -16,12 +15,11 @@ export default function AllRecipe() {
 
         return (
                 <div>
-                        <SearchBar/>
                         <h3> Encuentra Aqui tu receta favorita</h3>
                         <h6>Añadela al calendario y organiza tu semana</h6>
                         
                         <MidNav />
-                <Cards allRecipes={allRecipes}/>       
+                        <Cards allRecipes={allRecipes}/>       
                 </div>
         )
 }
