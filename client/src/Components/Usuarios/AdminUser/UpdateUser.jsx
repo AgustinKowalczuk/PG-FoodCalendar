92fe6247dc80c00c4f5d74ed487a2792f2540d06
Jocho94 <jocho1990@hotmail.com>
2021-09-19 19:45:05 -0300

    asd

---
+++ client/src/Components/Usuarios/AdminUser/UpdateUser.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateUser } from "../../../actions";
+
+export default function UpdateUser ({id, category}) {
+
+    const dispatch = useDispatch();
+    const token = useSelector((state) => state.token);
+
+    function handleClick(){
+        const ok = window.confirm('Deseas cambiar de categoría a este usuario?')
+        if(ok){
+            if(category === 'User'){
+                category= 'Admin';
+            } else if(category === 'Admin'){
+                category= 'User';
+            }
+            console.log(category)
+        dispatch(updateUser(id,{category}, token))
+        }
+    }
+    return ( 
+        <div>
+            <button className="btn btn-danger" onClick={() => handleClick(id)}>Cambiar categoría</button>
+        </div>
+     );
+}
