cf19f70021d398a18d7e2475a25770bda2bd2f12
Kowal64 <kowalczukagustin@gmail.com>
2021-09-24 03:04:22 -0300

    Pablo me dijo

--- api/controller/emailUtils.js
+++ api/controller/emailUtils.js
@@ -3,29 +3,29 @@ const nodemailer = require('nodemailer');
 const { env: { USER_MAIL, PASSWORD_MAIL } } = process;
 
 const transportEmail = async (email, html, subject) => {
-    const transport = nodemailer.createTransport({
-        host: 'smtp.mailtrap.io',
-        port: 2525,
-        auth: {
-            user: '0533c2835ff75f',
-            pass: '4db962f25944ba'
-        }
-    });
     // const transport = nodemailer.createTransport({
-    //     host: 'smtp.gmail.com',
-    //     port: 465,
+    //     host: 'smtp.mailtrap.io',
+    //     port: 2525,
     //     auth: {
-    //        user: USER_MAIL,
-    //        pass: PASSWORD_MAIL
+    //         user: '0533c2835ff75f',
+    //         pass: '4db962f25944ba'
     //     }
     // });
+    const transport = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        auth: {
+            user: USER_MAIL,
+            pass: PASSWORD_MAIL
+        }
+    });
     const message = {
         from: USER_MAIL, // Sender address
         to: email, // List of recipients
         subject: `Recipes Calendar - ${subject}`, // Subject line
         html: html // html head and body
     };
-    await transport.sendMail(message, function(err, info) {
+    await transport.sendMail(message, function (err, info) {
         if (err) {
             console.log(err);
             throw err;
@@ -38,7 +38,7 @@ const transportEmail = async (email, html, subject) => {
 const htmlReplacer = (oldText, newText) => {
     const obj = {}
     oldText.forEach((e, i) => obj[e] = newText[i]);
-    const re = new RegExp(Object.keys(obj).join("|"),"gi");
+    const re = new RegExp(Object.keys(obj).join("|"), "gi");
     return [re, obj];
 }
 
