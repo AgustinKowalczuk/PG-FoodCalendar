59c4d11b4146a0b3c15c254d05146aa09c32fca9
Pablo Agudo <84361788+PabloIVAgudo@users.noreply.github.com>
2021-09-24 00:21:18 -0300

    Revert "2 "change searchbar, more warnings, delete all review (Back and Front)"""""""

--- client/src/Components/CardRelacionadas/CardRelacionadas.jsx
+++ client/src/Components/CardRelacionadas/CardRelacionadas.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import style from '../../Styles/StyleCards.module.css';
-import { FilterRecipeByCategory } from '../../actions/index';
-import Cards from '../Cards/Cards';
-
+import { FilterRecipeByCategory } from '../../actions/index'
+import Cards from '../Cards/Cards'
+import InventaryNav from '../Inventary/InventaryNav'
 export default function CardRelacionadas() {
     const allRecipes = useSelector((state) => state.recipes)
     const token = useSelector(state => state.token);
