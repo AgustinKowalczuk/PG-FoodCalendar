98e91906eb0413aa2e17638681884304b74d7f89
Pablo Agudo <84361788+PabloIVAgudo@users.noreply.github.com>
2021-09-23 23:43:14 -0300

    Revert "Revert "change searchbar, more warnings, delete all review (Back and Front)""

--- client/src/Components/SearchBar/SearchBar.jsx
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux"
 import { useState } from "react";
 import { searchRecipes } from "../../actions/index"
@@ -10,30 +10,40 @@ import swal from 'sweetalert';
 export default function SearchBar() {
   const token = useSelector(state => state.token);
   const [input, setInput] = useState("");
+  const history = useHistory()
 
-  const handleInputChange = (e) => {  
+  const handleInputChange = (e) => {
     setInput(e.target.value)
   }
 
   const dispatch = useDispatch();
 
-  const handleSearch = ( e )=> {
-    if(input === "") return swal({
+  const handleSearch = (e) => {
+    if (input === "") return swal({
       title: "Campo incompleto",
       text: "EL campo esta vacio",
       icon: "error",
       button: "Aceptar",
-  })
-    dispatch(searchRecipes(input,token))  
+    })
+    dispatch(searchRecipes(input, token))
+    history.push(`/search/${input}`)
+    setInput("")
   }
 
   return (
-    <div class="d-flex">
+    <form id={styles.form} onSubmit={(e) => {
+      e.preventDefault();
+      handleSearch(input);
+    }}>
       <input
-        class="form-control me-2" id={styles.SearchBar} type="text" placeholder="Buscar" onChange={(e) => handleInputChange(e)} /> 
-      <Link id={styles.button} onClick={(e) => handleSearch(e)} class="btn btn-outline-success" to={`/search/${input}`} >
-         Buscar
-      </Link>
-    </div>
+        type="text"
+        placeholder="Buscar..."
+        value={input}
+        onChange={e => handleInputChange(e)}
+      />
+      <button type="submit" class="btn btn-outline-success" id={styles.button}>
+        Buscar
+      </button>
+    </form>
   );
 }
