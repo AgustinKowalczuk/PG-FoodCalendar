9ad638c631b638c2cfaf39a46523d45ff270d46e
Jocho94 <jocho1990@hotmail.com>
2021-09-24 02:44:59 -0300

    SearchBar, SlideImag, Mis Calendarios

--- client/src/Components/Nav/SidebarData.js
+++ client/src/Components/Nav/SidebarData.js
@@ -14,136 +14,145 @@ import * as FaIcons from 'react-icons/fa';
 
 export const SidebarDataNotUser = [
     {
-        title:'Home',
+        title: 'Home',
         path: '/',
-        icon: <AiIcons.AiFillHome/>,
-        cName:'nav-text'
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
     },
     {
-        title:'Todas las Recetas',
+        title: 'Todas las Recetas',
         path: '/AllRecipe',
-        icon: <BiIcons.BiBookHeart/>,
-        cName:'nav-text'
+        icon: <BiIcons.BiBookHeart />,
+        cName: 'nav-text'
     },
     {
-        title:'Crea tu calendario',
+        title: 'Crea tu calendario',
         path: '/shop',
-        icon: <IoIcons.IoCreateOutline/>,
-        cName:'nav-text'
+        icon: <IoIcons.IoCreateOutline />,
+        cName: 'nav-text'
     },
 
 ]
 
-export const SidebarDataUser= [
-    
-    {
-        title:'Home',
-        path: '/',
-        icon: <AiIcons.AiFillHome/>,
-        cName:'nav-text'
-    },
-    {
-        title:'Todas las Recetas',
-        path: '/AllRecipe',
-        icon: <BiIcons.BiBookHeart/>,
-        cName:'nav-text'
-    },
-    {
-        title:'Crear Receta',
-        path: '/create/recipe',
-        icon: <BsIcons.BsPencilSquare/>,
-        cName:'nav-text'
-    },
-    {
-        title:'Crea tu calendario',
-        path: '/shop',
-        icon: <BiIcons.BiCalendarHeart/>,
-        cName:'nav-text'
-    },
-   
-   
+export const SidebarDataUser = [
 
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Todas las Recetas',
+        path: '/AllRecipe',
+        icon: <BiIcons.BiBookHeart />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Crear Receta',
+        path: '/create/recipe',
+        icon: <BsIcons.BsPencilSquare />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Crea tu calendario',
+        path: '/shop',
+        icon: <BiIcons.BiCalendarHeart />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Mis calendarios',
+        path: '/calendar/user',
+        icon: <BiIcons.BiCalendarHeart />,
+        cName: 'nav-text'
+    }
 ]
 
 export const SidebarDataAdmin = [
-    
-    
+
+
     {
-        title:'Home',
+        title: 'Home',
         path: '/',
-        icon: <AiIcons.AiFillHome/>,
-        cName:'nav-text'
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
     },
     {
-        title:'Todas las Recetas',
+        title: 'Todas las Recetas',
         path: '/AllRecipe',
-        icon: <BiIcons.BiBookHeart/>,
-        cName:'nav-text'
+        icon: <BiIcons.BiBookHeart />,
+        cName: 'nav-text'
     },
-    
+
     {
-        title:'Gestion Calendario',
-        icon: <AiIcons.AiFillSetting/>,
-        path:'#',
-        cName:'nav-text',
+        title: 'Gestion Calendario',
+        icon: <AiIcons.AiFillSetting />,
+        path: '#',
+        cName: 'nav-text',
         iconClosed: <RiIcons.RiArrowDownSFill />,
         iconOpened: <RiIcons.RiArrowUpSFill />,
         subNav: [
             {
-                title:'Crear Receta',
+                title: 'Crear Receta',
                 path: '/create/recipe',
-                icon: <BsIcons.BsPencilSquare/>,
-                cName:'nav-text'
+                icon: <BsIcons.BsPencilSquare />,
+                cName: 'nav-text'
             },
             {
-                title:'Crea tu calendario',
+                title: 'Crea tu calendario',
                 path: '/shop',
-                icon: <BiIcons.BiCalendarHeart/>,
-                cName:'nav-text'
+                icon: <BiIcons.BiCalendarHeart />,
+                cName: 'nav-text'
             },
-          ]
-        },
- 
-    
+            {
+                title: 'Mis calendarios',
+                path: '/calendar/user',
+                icon: <BiIcons.BiCalendarHeart />,
+                cName: 'nav-text'
+            }
+        ]
+    },
+
+
     {
-        title:'Gestion',
-        icon: <AiIcons.AiFillSetting/>,
-        path:'#',
-        cName:'nav-text',
+        title: 'Gestion',
+        icon: <AiIcons.AiFillSetting />,
+        path: '#',
+        cName: 'nav-text',
         iconClosed: <RiIcons.RiArrowDownSFill />,
         iconOpened: <RiIcons.RiArrowUpSFill />,
         subNav: [
             {
-                title:'Todos los Calendarios',
+                title: 'Todos los Calendarios',
                 path: '/calendar',
-                icon: <FaIcons.FaRegCalendarAlt/>,
-                cName:'nav-text'
+                icon: <FaIcons.FaRegCalendarAlt />,
+                cName: 'nav-text'
             },
             {
-                title:'Todos los usuarios',
+                title: 'Todos los usuarios',
                 path: '/user',
-                icon: <IoIcons.IoListCircleOutline/>,
-                cName:'nav-text'
+                icon: <IoIcons.IoListCircleOutline />,
+                cName: 'nav-text'
             },
             {
-                title:'Recetas deshabilitadas',
+                title: 'Recetas deshabilitadas',
                 path: '/availability',
-                icon: <IoIcons.IoListCircleOutline/>,
-                cName:'nav-text'
+                icon: <IoIcons.IoListCircleOutline />,
+                cName: 'nav-text'
             },
-          ]
+        ]
     },
-    
-    
-    
+
+
+
 
 ]
 
 export const filterData = (token, user) => {
-    if(!!token && user.category !== 'Admin'){  
+    if (!!token && user.category !== 'Admin') {
         return SidebarDataUser;
     }
-    if(!!token && user.category === 'Admin'){    
+    if (!!token && user.category === 'Admin') {
         return SidebarDataAdmin
     }
     return SidebarDataNotUser
